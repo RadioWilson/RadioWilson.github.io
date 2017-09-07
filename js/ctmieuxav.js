443b2daf0c2bb184cc4fd4ce4948a5312473a52c
DindieWestWood <drouet.dimitri@gmail.com>
2017-09-08 01:46:39 +0200

    C'était Mieux Avant Episode 1

---
+++ js/ctmieuxav.js
@@ -0,0 +1,7 @@
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    document.getElementById("ctmieuxav-e000-played").innerHTML = "10928";
+    document.getElementById("ctmieuxav-e001-played").innerHTML = "8635";
+    
+});
